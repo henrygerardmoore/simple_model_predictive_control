3303c608cab7be4878b29e7d84cf135e4b6cdd33
Henry Moore <henrygerardmoore@gmail.com>
2025-12-10 18:09:16 -0700

    fix num iter

--- src/dynamics_optimizer.rs
+++ src/dynamics_optimizer.rs
@@ -902,7 +902,7 @@ mod bench {
         #[cfg(debug_assertions)]
         let num_iterations = 10;
         #[cfg(not(debug_assertions))]
-        let num_iterations = 1000;
+        let num_iterations = 10000;
 
         let (_, dynamics_optimizer) = get_simple_optimizer(goal.clone());
         let root_id = dynamics_optimizer.dynamics_tree.root().id();
